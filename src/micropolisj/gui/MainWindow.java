890cff2474788e060da1d492e46e18a29d84857f
Blythe <bweng@andrew.cmu.edu>
2020-04-02 23:07:50 -0400

    Adding the needed components to initialize my tool

--- src/micropolisj/gui/MainWindow.java
+++ src/micropolisj/gui/MainWindow.java
@@ -1046,11 +1046,19 @@ public class MainWindow extends JFrame
 		gridBox.add(b6,c);
 
 		b6.add(makeToolBtn(MicropolisTool.AIRPORT));
+		
+		c.gridy++;
+		Box b7 = new Box(BoxLayout.X_AXIS);
+		gridBox.add(b7,c);
+		b7.add(makeToolBtn(MicropolisTool.SOLAR));
+
 
 		// add glue to make all elements align toward top
 		c.gridy++;
 		c.weighty = 1.0;
 		gridBox.add(new JLabel(), c);
+		
+		
 
 		return toolBar;
 	}
